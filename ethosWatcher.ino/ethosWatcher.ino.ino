d2be40ccc708dc004fd95d36c648c45b16c973c4
Thomas Irmscher <thomas.irmscher@gmail.com>
2018-03-03 10:34:11 +0100

    add user_interface.h

--- ethosWatcher.ino/ethosWatcher.ino.ino
+++ ethosWatcher.ino/ethosWatcher.ino.ino
@@ -6,6 +6,9 @@
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <SPI.h>
+extern "C" {
+  #include "user_interface.h"
+}
 
 const int GPIO12_green  = 12;
 const int GPIO13_blue   = 13;
@@ -20,6 +23,8 @@ const int calltime_ms = 10000;
 char ssid[]     = "WLAN-987745";
 char password[] = "8619417772260054";
 char servername[] = "tombou.ethosdistro.com";
+char wiFiHostname[ ] = "ethoswatcher";
+
 char rig[] = "49c27d";
 int status = WL_IDLE_STATUS;
 float oldhash = 0;
@@ -29,6 +34,17 @@ WiFiClient client;
 int reboots = 0;
 bool hashOK = 0;
 bool minerHasBootedUp = 0;
+float ethosHash_web = 0;
+
+
+//webserver----------------------------------
+// Set web server port number to 80
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
+
+//int alle_x_sekunden=10;
 
 void setup() {  
  // Initialize Serial port
@@ -61,6 +77,9 @@ void setup() {
   digitalWrite(GPIO15_red, LOW);
 
   reboot();
+
+  //Timer1.initialize(alle_x_sekunden*1000000);
+  //Timer1.attachInterrupt(doLoop);
 }
 
 void connectWifi(){
@@ -69,6 +88,10 @@ void connectWifi(){
   Serial.print("            ");
   Serial.print(ssid);
   status = WiFi.begin(ssid, password);
+  
+  wifi_station_set_auto_connect(true);
+  wifi_station_set_hostname(wiFiHostname);
+  
   int count =1;
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -155,9 +178,42 @@ float parseJson(){
   return rigs_hash;
 }
 
+void doLoop(){
+  float ethosHash = getEthosHash();
+  ethosHash_web = ethosHash;
+  if ((float)ethosHash != (float)oldhash){
+    Serial.println("");
+    Serial.print("Hash: ");
+    Serial.print(ethosHash);
+    oldhash = ethosHash;
+  } else{
+    Serial.print(".");
+  }
+  
+  //Serial.println("--loop start--");
+  // checks the hash value to be over threshold at least once (booting)
+  if (minerHasBootedUp == 0){
+    if ((float)ethosHash >= (float)threshold){
+      hashOK = true;
+      minerHasBootedUp = 1;
+    } 
+    indicateNOK();
+  }else{
+    if ((float)ethosHash >= (float)threshold){
+      hashOK = 1;
+      indicateOK(); // fast green blinking
+    } else{
+      hashOK = 0;
+      minerHasBootedUp = 0; //back to beginning
+      reboot(); //blue is rebooting, red is indicating reboot counter
+    }
+  }
+}
 
 void loop() {
-  float ethosHash = getEthosHash();
+  //doLoop();
+  /*float ethosHash = getEthosHash();
+  ethosHash_web = ethosHash;
   if ((float)ethosHash != (float)oldhash){
     Serial.println("");
     Serial.print("Hash: ");
@@ -188,7 +244,139 @@ void loop() {
   
   //repeat after 10 seconds
   delay(calltime_ms);
-  
+  */
+  webserver();
+}
+
+void webserver(){
+  //http://randomnerdtutorials.com/esp8266-web-server-with-arduino-ide/
+  //Serial.println(".");
+  //Serial.println("..");
+  //Serial.println("...");
+  //Serial.println("....");
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+  Serial.println("found");
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          if (currentLine.length() == 0) {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+/*            
+            // turns the GPIOs on and off
+            if (header.indexOf("GET /5/on") >= 0) {
+              Serial.println("GPIO 5 on");
+              output5State = "on";
+              digitalWrite(output5, HIGH);
+            } else if (header.indexOf("GET /5/off") >= 0) {
+              Serial.println("GPIO 5 off");
+              output5State = "off";
+              digitalWrite(output5, LOW);
+            } else if (header.indexOf("GET /4/on") >= 0) {
+              Serial.println("GPIO 4 on");
+              output4State = "on";
+              digitalWrite(output4, HIGH);
+            } else if (header.indexOf("GET /4/off") >= 0) {
+              Serial.println("GPIO 4 off");
+              output4State = "off";
+              digitalWrite(output4, LOW);
+            }
+*/          
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #77878A;}</style></head>");
+            client.println("<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css\">");
+
+            client.println("<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js\"></script>");
+
+            
+            // Web Page Heading
+            client.println("<body><h1>ESP8266 Web Server</h1>");
+            
+            // Display current state, and ON/OFF buttons for GPIO 5  
+            client.print("<p>threshold: ");
+            client.print(threshold);
+            client.println("</p>");
+            
+            client.print("<p>rig: ");
+            client.print(rig);
+            client.println("</p>");
+            
+            client.print("<p>ssid: ");
+            client.print(ssid );
+            client.println("</p>");
+            
+            client.print("<p>servername: ");
+            client.print(servername);
+            client.println("</p>");
+            
+            client.print("<p>reboots: ");
+            client.print(reboots);
+            client.println("</p>");
+            
+            client.print("<p>hashOK: ");
+            client.print(hashOK);
+            client.println("</p>");
+
+            client.print("<p>minerHasBootedUp: ");
+            client.print(minerHasBootedUp);
+            client.println("</p>");
+            
+            client.print("<p>ethosHash: ");
+            client.print(ethosHash_web);
+            client.println("</p>");
+            
+            
+            /*
+            // If the output5State is off, it displays the ON button       
+            if (output5State=="off") {
+              client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
+            } 
+               
+            // Display current state, and ON/OFF buttons for GPIO 4  
+            client.println("<p>GPIO 4 - State " + output4State + "</p>");
+            // If the output4State is off, it displays the ON button       
+            if (output4State=="off") {
+              client.println("<p><a href=\"/4/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/4/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+            */
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+  }
 }
 
 void indicateOK(){
